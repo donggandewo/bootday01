bf4b8826691d6d846e7d2e20afa2c4d2b3abfb48
zhang <boisi@qq.com>
2019-04-12 09:59:14 +0800

    UserEntity v2.0

--- src/main/java/com/baizhi/entity/User.java
+++ src/main/java/com/baizhi/entity/User.java
@@ -3,10 +3,13 @@ package com.baizhi.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 public class User {
     private String username;
+    @Id
     private int id;
     private String password;
 }
